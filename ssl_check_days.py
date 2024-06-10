4f6d2180b7afae5bd44b358e08684e4297cbdbbe
mistergrib <54153868+mistergrib@users.noreply.github.com>
2024-06-10 21:08:21 +0300

    Update ssl_check_days.py
    new version
    error processing added

--- ssl_check_days.py
+++ ssl_check_days.py
@@ -16,11 +16,21 @@ tg_group = os.getenv("CHAT_ID")
 
 def get_cert_expiry(hostname, port=443):
     context = ssl.create_default_context()
-    with socket.create_connection((hostname, port)) as sock:
-        with context.wrap_socket(sock, server_hostname=hostname) as ssock:
-            cert = ssock.getpeercert()
-            expiry_date = datetime.strptime(cert["notAfter"], "%b %d %H:%M:%S %Y %Z")
-            return expiry_date
+    try:
+        with socket.create_connection((hostname, port)) as sock:
+            with context.wrap_socket(sock, server_hostname=hostname) as ssock:
+                cert = ssock.getpeercert()
+                expiry_date = datetime.strptime(cert["notAfter"], "%b %d %H:%M:%S %Y %Z")
+                return expiry_date
+    except ssl.SSLCertVerificationError as e:
+        print(f"Certificate was expired for {hostname}: {e}")
+        return f"Certificate was expired for {hostname}: {e}"
+    except ConnectionRefusedError as e:
+        print(f"Can't connect to {hostname}: {e}")
+        return f"Can't connect to {hostname}: {e}"
+    except Exception as e:
+        print(f"We got an error: {e}")
+        return f"We got an error with {hostname}: {e}"
 
 
 def send_tg_message(token, chat_id, alert):
@@ -41,10 +51,17 @@ def send_tg_message(token, chat_id, alert):
         print(f"Error sending message: {e}")
 
 
-
+full_message = ""
 for domain in domains:
     today = datetime.utcnow()
-    days_remaining = (get_cert_expiry(domain) - today).days
-    if days_remaining <= int(days_threshold):
-        message = f"{domain} expire in {days_remaining} days"
-        send_tg_message(tg_bot_token, tg_group, message)
+    cert_date = get_cert_expiry(domain)
+    if isinstance(cert_date, datetime):
+        days_remaining = (cert_date - today).days
+        if days_remaining <= int(days_threshold):
+            message = f"{domain} expire in {days_remaining} days\n"
+            full_message += message
+    else:
+        full_message = full_message + cert_date + "\n"
+
+
+send_tg_message(tg_bot_token, tg_group, full_message)
