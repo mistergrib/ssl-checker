22eb6da2995034d03c5934a7490392231b029c08
mistergrib <54153868+mistergrib@users.noreply.github.com>
2024-06-12 18:14:46 +0300

    Update ssl_check_days.py
    added email notifications

--- ssl_check_days.py
+++ ssl_check_days.py
@@ -4,14 +4,28 @@ from datetime import datetime
 import requests
 import os
 from dotenv import load_dotenv
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 load_dotenv()
 # Params, variables, etc
 domains_env = os.getenv("HOSTNAMES")
 domains = domains_env.split(",")
 days_threshold = os.getenv("DAYS_THRESHOLD")
+tg_alerts_enabled = bool(os.getenv("TELEGRAM_ALERTS_ENABLED").lower() == "true")
+email_alerts_enabled = bool(os.getenv("EMAIL_ALERTS_ENABLED").lower() == "true")
 tg_bot_token = os.getenv("BOT_TOKEN")
 tg_group = os.getenv("CHAT_ID")
+smtp_server = os.getenv("SMTP_SERVER")
+smtp_port = os.getenv("SMTP_PORT")
+smtp_STARTTLS_enabled = bool(os.getenv("SMTP_STARTTLS_ENABLED").lower() == "true")
+smtp_SSL_enabled = bool(os.getenv("SMTP_SSL_ENABLED").lower() == "true")
+email_login = os.getenv("EMAIL_LOGIN")
+email_from_address = os.getenv("EMAIL_FROM_ADDRESSES")
+email_password = os.getenv("EMAIL_PASSWORD")
+email_subject = os.getenv("EMAIL_SUBJECT")
+recipient_email = os.getenv("EMAIL_RECIPIENT")
 
 
 def get_cert_expiry(hostname, port=443):
@@ -43,12 +57,44 @@ def send_tg_message(token, chat_id, alert):
     try:
         response = requests.post(url, data=payload)
         if response.status_code == 200:
-            print("Message sent successfully")
-            print(message)
+            print(f"Message sent successfully:\n{full_message}")
         else:
             print(f"Failed to send message: {response.status_code} - {response.text}")
     except Exception as e:
         print(f"Error sending message: {e}")
+
+
+def send_email(server, port):
+    try:
+        msg = MIMEMultipart()
+        msg['From'] = email_login
+        msg['To'] = recipient_email
+        msg['Subject'] = email_subject
+        msg.attach(MIMEText(full_message, 'plain'))
+
+        if smtp_STARTTLS_enabled and smtp_SSL_enabled:
+            print("you should check one of the SMTP_STARTTLS_ENABLED or SMTP_SSL_ENABLED")
+        try:
+            if smtp_STARTTLS_enabled:
+                smtp = smtplib.SMTP(server, port)
+                smtp.starttls()
+        except Exception as e:
+            print(e)
+        try:
+            if smtp_SSL_enabled:
+                smtp = smtplib.SMTP_SSL(server, port)
+        except Exception as e:
+            print(e)
+        if not smtp_SSL_enabled and not smtp_STARTTLS_enabled:
+            smtp = smtplib.SMTP(smtp_server, smtp_port)
+
+        smtp.login(email_login, email_password)
+        smtp.send_message(msg)
+
+    except Exception as e:
+        print(f"Can't send email: {e}")
+    finally:
+        smtp.quit()
 
 
 full_message = ""
@@ -64,4 +110,7 @@ for domain in domains:
         full_message = full_message + cert_date + "\n"
 
 
-send_tg_message(tg_bot_token, tg_group, full_message)
+if tg_alerts_enabled:
+    send_tg_message(tg_bot_token, tg_group, full_message)
+if email_alerts_enabled:
+    send_email(smtp_server, smtp_port)
